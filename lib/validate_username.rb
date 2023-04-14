d9dba6d2b8fa8211f842e6e00ab6e24e9769d8ba
Christian Bäuerlein <fabrik42@gmail.com>
2023-04-14 10:24:45 +0200

    Add some acceptance criteria and tests plus refactorable implementation

--- lib/validate_username.rb
+++ lib/validate_username.rb
@@ -1,3 +1,10 @@
+# todo refactor as you go
 def validate_username(username)
-  return true
+  return false if username.size == 0
+
+  return false if username.size < 3
+
+  return false if username.size > 30
+
+  true
 end
--- spec/validate_username_spec.rb
+++ spec/validate_username_spec.rb
@@ -1,16 +1,38 @@
 require './lib/validate_username'
 
 describe 'validate_username' do
-  it 'allows a valid username' do
+  it 'does allow a valid username' do
     result = validate_username('bob')
-    expect(result).to be true
+    expect(result).to be(true)
   end
 
-  # xit 'does not allow an empty username' do
-  #     result = validate_username('')
-  #     expect(result).to be false
-  # end
+  it 'does not allow an empty username' do
+    result = validate_username('')
+    expect(result).to be(false)
+  end
 
-  # xit 'does not allow usernames shorter than 3 chars'
-  # xit 'does not allow usernames longer than 30 chars'
+  it 'does not allow usernames shorter than 3 chars' do
+    result = validate_username('aa')
+    expect(result).to be(false)
+
+    result = validate_username('aaa')
+    expect(result).to be(true)
+  end
+
+  it 'does not allow usernames longer than 30 chars' do
+    result = validate_username('a' * 31)
+    expect(result).to be(false)
+
+    result = validate_username('a' * 30)
+    expect(result).to be(true)
+  end
+
+  # Acceptance Criteria for valid username:
+  # - allows simple and clear username
+  # - does not allow an empty username
+  # - does not allow username shorter than 3 chars
+  # - does not allow username longer than 30 chars
+  # - optional: does not allow numbers
+  #
+  # remember: Red - Green - Refactor!
 end
